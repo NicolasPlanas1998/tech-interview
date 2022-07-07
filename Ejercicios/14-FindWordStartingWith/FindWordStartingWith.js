05e1dd3fefe1ae6e7244915d04a8d9eded9b137c
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-07-07 10:39:59 -0300

    Solucion ejercicio 14

--- Ejercicios/14-FindWordStartingWith/FindWordStartingWith.js
+++ Ejercicios/14-FindWordStartingWith/FindWordStartingWith.js
@@ -1,6 +1,23 @@
 function FindWordStartingWith (book, query) {
-  // Your code here:
 
+  const text = book.text.toLowerCase() // Para que sea case insensitive
+  const result = []
+
+  for(let i = 0 ; i < text.length ; i ++){
+    // me va a ir cortando para poder comparar con query
+    const sliced = text.slice(i, i+ query.length) 
+   //Pongo estas condiciones porque solo quiero saber de aquellas palabras que arrancan con ese query
+    if(sliced === query && (text[i -1] === " " || i === 0)) result.push(i)
+    
+  }
+  return result
 }
+
+const book = {
+  id: 1,
+  text: "Erase una vez un libro de palabras que era un poco aburrido pero tenia mucho aunque algunas palabras locas",
+};
+
+console.log(FindWordStartingWith(book, "un"))
 
 module.exports = FindWordStartingWith
