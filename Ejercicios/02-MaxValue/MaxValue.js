6b2dd0dfe84f1c3f32e0a2947bd5f22389ed1f89
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-09-02 11:57:42 -0300

    Max Value TS solution

--- Ejercicios/02-MaxValue/MaxValue.js
+++ Ejercicios/02-MaxValue/MaxValue.js
@@ -1,28 +1,19 @@
 function MaxValue(shares) {
-    // Your code here:
-    return 2;
+    // Your code here:                                //Complejidad
+    var arrSums = []; //1  
+    var index = shares.length - 1; //1
+    for (index; 0 <= index; index--) { //n
+        for (var j = index; 0 <= j; j--) { //n^2
+            arrSums.push(shares[index] - shares[j]);
+        }
+    }
+    var maxProfit = arrSums.reduce(function (ac, el) {
+        return Math.max(ac, el);
+    });
+    return maxProfit;
 }
 console.log(MaxValue([4, 3, 2, 5, 11]));
 // output :9
 console.log(MaxValue([23, 7, 3, 4, 8, 6]));
 // output :5
-//   function MaxValue (shares) {
-//     // Your code here:                                //Complejidad
-//     let arrSums = []                                  //1  
-//     let index = shares.length -1                      //1
-//     for(index; 0 <= index; index--){                  //n
-//       for(let j = index; 0 <= j; j--){                //n^2
-//         arrSums.push(shares[index]-shares[j])
-//       }
-//     }
-//     let maxProfit = arrSums.reduce((ac, el)=>{
-//       return Math.max(ac,el)
-//     })
-//     return maxProfit
-//   }
-//   console.log(MaxValue([4,3,2,5,11]));
-//   //Output: 9 
-//   console.log(MaxValue([23,7,3,4,8,6]));
-//   // Output: 5
-//   module.exports = MaxValue
 module.exports = MaxValue;
--- Ejercicios/02-MaxValue/MaxValue.ts
+++ Ejercicios/02-MaxValue/MaxValue.ts
@@ -1,6 +1,18 @@
 function MaxValue (shares:number[]):number  {
-    // Your code here:
-    return 2
+   // Your code here:                                //Complejidad
+    let arrSums:number[] = []                                  //1  
+    let index:number = shares.length -1                      //1
+
+    for(index; 0 <= index; index--){                  //n
+      for(let j = index; 0 <= j; j--){                //n^2
+        arrSums.push(shares[index]-shares[j])
+      }
+    }
+  
+    let maxProfit:number = arrSums.reduce((ac, el)=>{
+      return Math.max(ac,el)
+    })
+    return maxProfit
   }
   
 
@@ -9,27 +21,5 @@ function MaxValue (shares:number[]):number  {
   console.log(MaxValue([23, 7, 3, 4, 8, 6]));
   // output :5
   
-//   function MaxValue (shares) {
-//     // Your code here:                                //Complejidad
-//     let arrSums = []                                  //1  
-//     let index = shares.length -1                      //1
-//     for(index; 0 <= index; index--){                  //n
-//       for(let j = index; 0 <= j; j--){                //n^2
-//         arrSums.push(shares[index]-shares[j])
-//       }
-//     }
-  
-//     let maxProfit = arrSums.reduce((ac, el)=>{
-//       return Math.max(ac,el)
-//     })
-//     return maxProfit
-//   }
-  
-  
-//   console.log(MaxValue([4,3,2,5,11]));
-//   //Output: 9 
-//   console.log(MaxValue([23,7,3,4,8,6]));
-//   // Output: 5
-//   module.exports = MaxValue
   
   module.exports = MaxValue
