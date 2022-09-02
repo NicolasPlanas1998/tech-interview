bc33bc12c9a3a7e2630554d3429e2f15ad6bb185
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-09-02 11:57:04 -0300

    TypeScript integration

--- Ejercicios/02-MaxValue/MaxValue.ts
+++ Ejercicios/02-MaxValue/MaxValue.ts
@@ -1,11 +1,14 @@
-function MaxValue(shares) {
+function MaxValue (shares:number[]):number  {
     // Your code here:
-    return 2;
-}
-console.log(MaxValue([4, 3, 2, 5, 11]));
-// output :9
-console.log(MaxValue([23, 7, 3, 4, 8, 6]));
-// output :5
+    return 2
+  }
+  
+
+  console.log(MaxValue([4, 3, 2, 5, 11]));
+  // output :9
+  console.log(MaxValue([23, 7, 3, 4, 8, 6]));
+  // output :5
+  
 //   function MaxValue (shares) {
 //     // Your code here:                                //Complejidad
 //     let arrSums = []                                  //1  
@@ -15,14 +18,18 @@ console.log(MaxValue([23, 7, 3, 4, 8, 6]));
 //         arrSums.push(shares[index]-shares[j])
 //       }
 //     }
+  
 //     let maxProfit = arrSums.reduce((ac, el)=>{
 //       return Math.max(ac,el)
 //     })
 //     return maxProfit
 //   }
+  
+  
 //   console.log(MaxValue([4,3,2,5,11]));
 //   //Output: 9 
 //   console.log(MaxValue([23,7,3,4,8,6]));
 //   // Output: 5
 //   module.exports = MaxValue
-module.exports = MaxValue;
+  
+  module.exports = MaxValue
