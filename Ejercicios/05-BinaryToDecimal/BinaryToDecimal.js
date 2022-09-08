d3f53fa70b01fdaeeb32806a11189beaed98dc6f
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-09-08 10:21:42 -0300

    BinaryToDecimal Resolved

--- Ejercicios/05-BinaryToDecimal/BinaryToDecimal.js
+++ Ejercicios/05-BinaryToDecimal/BinaryToDecimal.js
@@ -1,6 +1,17 @@
 function BinaryToDecimal (binary) {
-  // Your code here:
+  let binaryLength = binary.length
+  console.log(binaryLength);
+  let arrBinary = binary.split("")
+  console.log(arrBinary);
 
+  for (let i =arrBinary.length; i < arrBinary.length; i++) {
+    const element = array[i];
+    
+  }
 }
 
-module.exports = BinaryToDecimal
+
+    console.log(BinaryToDecimal("10"));
+    // expect 2
+    // console.log(BinaryToDecimal("111")); 
+    // expect 7
