3a5cb9b493898274398bd7a6fa4c30226688a471
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-09-09 09:54:40 -0300

    TwoSum Resolved

--- LeetCode/TwoSum/TwoSum.js
+++ LeetCode/TwoSum/TwoSum.js
@@ -20,29 +20,49 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
  
-var twoSum = function(nums, target) {
-    let index = []
+// var twoSum = function(nums, target) {
+//     let index = []
     
-    for(let i=0; i < nums.length; i++) {
-      let acc = 0
-      index.push(i)
-      acc = nums[i] //3 2 4
+//     for(let i=0; i < nums.length; i++) {
+//       let acc = 0
+//       index.push(i)
+//       acc = nums[i] //3 2 4
   
-      for(let j=1; j <= nums.length; j++){
-        acc += nums[j] // 3+2 
-        console.log(acc);
-        index.push(j)
-        if(acc > target) {
-          console.log(acc);
-            acc = 0
-            index = []
-        }
-        if(acc === target) return index
+//       for(let j=1; j <= nums.length; j++){
+//         acc += nums[j] // 3+2 
+//         console.log(acc);
+//         index.push(j)
+//         if(acc > target) {
+//           console.log(acc);
+//             acc = 0
+//             index = []
+//         }
+//         if(acc === target) return index
+//     }
+//     }
+//   };
+var twoSum = function(nums, target) {
+  let correctIndex = []
+  for (let i = 0; i < nums.length; i++) {
+    if(correctIndex.length === 2) return (correctIndex) 
+    
+    console.log(correctIndex);
+    secondIndex = i+1
+    for (secondIndex; secondIndex < nums.length; secondIndex++) {
+      let option = nums[i]+nums[secondIndex]
+      // console.log({i,j:secondIndex});
+      // console.log(option);
+      
+      if(option === target) {
+        correctIndex.push(i,secondIndex)
+        break
+      }
     }
-    }
+
+  }
   };
 
-  //! Esta mal el codigo, arreglar
   console.log(twoSum([2,7,11,15],9));
   console.log(twoSum([3,2,4],6));
   console.log(twoSum([3,3],6));
+  console.log(twoSum([2,5,5,11],10));
