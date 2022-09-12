d54b42b6dbc996b1b3558bfe5d17759d77448f55
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-09-12 10:13:59 -0300

    Intersection resolved

--- Ejercicios/07-Intersection/Intersection.js
+++ Ejercicios/07-Intersection/Intersection.js
@@ -1,6 +1,23 @@
 function intersection (arr1, arr2) {
   // Your code here:
+  let allArr = arr1.concat(arr2)
+  let uniqueValueArr = []
+  let repitedValue = []
+  for (let i = 0; i < allArr.length; i++) {
+    if(!uniqueValueArr.includes(allArr[i])) uniqueValueArr.push(allArr[i])
+    else repitedValue.push(allArr[i])
+  }
 
+  return repitedValue  
 }
+
+
+
+console.log(intersection([1,3, 5, 7, 10], [2, 3, 6, 8, 10, 20])) //([3, 10])
+console.log(intersection([7, 10, 3, 1, 5], [10, 6, 20, 3, 2, 8])) // 3
+console.log(intersection([1, 3, 4, 7], [2, 6, 8, 10, 20])) // []
+
+
+
 
 module.exports = intersection
