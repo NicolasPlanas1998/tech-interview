7cbf0281fcd9f3770576b4a8bc6a02035ac1392b
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-10-24 11:31:49 -0300

    Digital Root resolved

---
+++ CodeWar/RecursiveSum.js
@@ -0,0 +1,30 @@
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit,
+// continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1
+
+function digitalRoot(n) {
+    // ...
+    if(n.length === 1) return parseInt(n)
+    let array = [...n.toString()]
+    let total = array.reduce((ac,el)=>{
+        el = parseInt(el)
+        ac = parseInt(ac)
+        return ac+el
+    })
+    return digitalRoot(total)
+  }
+
+
+
+
+//! Best practice, its insane anyways cause he is not refactoring, just brilliant math
+// function digitalRoot(n) {
+//     return (n - 1) % 9 + 1;
+// }
